0b8f6227237774304c1ec947cd324308b694a06f
DoctorWaterLocal <75388424+DoctorWater@users.noreply.github.com>
2024-04-10 12:49:50 +0300

    gender added

--- NukeTest/Models/User.cs
+++ NukeTest/Models/User.cs
@@ -3,8 +3,11 @@ namespace NukeTest.Models;
 public class User
 {
     private string Name { get; set; }
+    private char Gender { get; set; }
+
     public User(string name)
     {
         Name = name;
+        Gender = 'M';
     }
 }
