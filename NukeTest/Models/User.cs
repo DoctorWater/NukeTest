63bb50fd8add102eed5302909ca7b1e728f38e50
DoctorWaterLocal <75388424+DoctorWater@users.noreply.github.com>
2024-04-10 13:20:50 +0300

    added surname

--- NukeTest/Models/User.cs
+++ NukeTest/Models/User.cs
@@ -5,11 +5,13 @@ public class User
     private string Name { get; set; }
     private char Gender { get; set; }
     private string Address { get; set; }
+    private string Surname { get; set; }
 
-    public User(string name, string address)
+    public User(string name, string surname, string address)
     {
         Name = name;
         Address = address;
+        Surname = surname;
         Gender = 'M';
     }
 }
