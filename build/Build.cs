8e0d093e046fe874042c1302489605495275858d
DoctorWaterLocal <75388424+DoctorWater@users.noreply.github.com>
2024-04-10 12:24:00 +0300

    build pipeline improved

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,61 @@
 using System;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
+using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+    
+    [Solution]      readonly Solution      Solution;
+    public static            int           Main() => Execute<Build>(x => x.Pack);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    Target Clean =>
+        _ => _
+            .Before(Restore)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetClean(s => s
+                    .SetProject(Solution));
+                EnsureCleanDirectory("artifacts");
+            });
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+    Target Restore =>
+        _ => _
+            .DependsOn(Clean)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetRestore(s => s
+                    .SetProjectFile(Solution));
+            });
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
+    Target Compile =>
+        _ => _
+            .DependsOn(Restore)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetBuild(s => s
+                    .SetProjectFile(Solution)
+                    .SetConfiguration(Configuration));
+            });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
-
+    Target Pack =>
+        _ => _
+            .DependsOn(Compile)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetPack(s => s
+                    .SetProject(Solution)
+                    .SetVersion(GitVersion.NuGetVersion)
+                    .SetConfiguration(Configuration)
+                    .SetOutputDirectory("artifacts"));
+            });
 }
